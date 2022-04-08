dbc5a3832b848a05416e7bc1f88f55558a9f0457
99061142 <99061142@mydavinci.nl>
2022-04-08 19:38:11 +0200

    Created a class to show the board
    Show the board of Tic-Tac-Toe, and functionality to click on the tile

--- python/new-code.py
+++ python/new-code.py
@@ -6,12 +6,12 @@ window = tk.Tk()
 window.title('Tic-Tac-Toe')
 window.configure(bg="lightgray")
 
-board = [[tk.StringVar()] * 3] * 3 # Board with the values inside
+board_list = [[tk.StringVar()] * 3] * 3 # Board with the values inside
 
 player = tk.StringVar() # Player that has the turn
 
 
-# Creates the homescreen
+# Show the homescreen
 class homescreen:
     def __init__(self, master) -> None:
         self.master = master # Master window
@@ -41,46 +41,57 @@ class homescreen:
         tk.Button(
             self.frame, 
             text="Submit", 
-            command=self.set_starting_player
+            command=self.chose_starting_player
         ).grid(columnspan=3, sticky="WE")
 
         self.frame.pack() # Add the frame to the window
 
 
-    def set_starting_player(self) -> None:
+    def chose_starting_player(self) -> None:
         chosen_player = player.get().upper() # Input of the user
 
         # If the user chose "x" or "o" as starting player
         if chosen_player == "X" or chosen_player == "O":
+            self.frame.destroy() # Destroy the frame of the homescreen
+            board(window) # Show the board
+
+
+# Show the board
+class board:
+    def __init__(self, master) -> None:
+        self.master = master # Master window
+        self.frame = tk.Frame(self.master) # Makes a new frame inside the master window
+
+        # Title
+        tk.Label(
+            self.frame, 
+            text="Tic-Tac-Toe", 
+            font=("Arial", 25)
+        ).grid(columnspan=3)
+
+        self.create_board()
+
+        self.frame.pack() # Add the frame to the window
+
+    def create_board(self) -> None:
+        # For every tile inside the board (3x3)
+        for row_index, row in enumerate(board_list):
+            for col_index, value in enumerate(row):
+                # Makes the tile
+                tile = tk.Button(
+                    self.frame, 
+                    borderwidth=2,
+                    relief="groove",
+                    text=value.get(),
+                    width=35,
+                    height=10,
+                    command= lambda: self.tile_pressed(value.get())
+                ).grid(row=row_index+1, column=col_index)
+
+    def tile_pressed(self, value):
+        # Tile is empty
+        if not value:
             pass
-
-
-"""
-# If the user pressed on an tile 
-def tile_pressed(value):
-    # If the tile was empty
-    if not value:
-        print("test")
-
-
-# Makes the board
-def make_board():
-    game_screen = tk.Frame(window)
-
-    # For every tile inside the board (3x3)
-    for row_index, row in enumerate(board):
-        for col_index, value in enumerate(row):
-            # Makes the tile
-            tile = tk.Button(
-                game_screen, 
-                borderwidth=2,
-                relief="groove",
-                text=value.get(),
-                width=35,
-                height=10,
-                command= lambda: tile_pressed(value.get())
-            ).grid(row=row_index+1, column=col_index)
-"""
 
 
 def main():
